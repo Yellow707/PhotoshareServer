31f4b825cd0ebfdf04c1e8361e6a762777516446
Igor Petrov <fox0234@yandex.ru>
2018-11-11 19:56:32 +0300

    init commit

---
+++ src/main/java/com/yellow/photoshare/PhotoshareServerApplication.java
@@ -0,0 +1,12 @@
+package com.yellow.photoshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoshareServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoshareServerApplication.class, args);
+    }
+}
