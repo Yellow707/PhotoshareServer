e978cf50d566f5ac936db8e10518a070baa556fe
Igor Petrov <fox0234@yandex.ru>
2018-11-13 23:21:40 +0300

    start implement Hibernate
    add UserEntity class for Hibernate
    update Gradle dependencies
    configure Hibernate on main class
    set application properties for database

---
+++ src/main/java/com/yellow/photoshare/entity/UserEntity.java
@@ -0,0 +1,80 @@
+package com.yellow.photoshare.entity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Users")
+public class UserEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "Name", length = 50, nullable = false)
+    private String name;
+
+    @Column(name = "Surname", length = 50, nullable = false)
+    private String surname;
+
+    @Column(name = "Username", length = 50, nullable = false)
+    private String username;
+
+    @Column(name = "Age", nullable = false)
+    private Integer age;
+
+    @Column(name = "Email", length = 128, nullable = false)
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
