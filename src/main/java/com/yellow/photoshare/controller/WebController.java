ffb16536ed3a509812d0e821742df5004788738b
Igor Petrov <fox0234@yandex.ru>
2018-12-04 22:14:24 +0300

    registration web form

--- src/main/java/com/yellow/photoshare/controller/WebController.java
+++ src/main/java/com/yellow/photoshare/controller/WebController.java
@@ -6,12 +6,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class GreetingController {
+public class WebController {
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
+    }
+
+    @GetMapping("/registration")
+    public String registration() {
+
+        return "registration";
+    }
+
+    @GetMapping("/registrationdone")
+    public String registrationRequest() {
+
+        return "registration";
     }
 
 }
