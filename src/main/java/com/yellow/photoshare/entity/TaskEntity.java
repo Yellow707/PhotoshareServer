a22eb744f93b6d734df8a72624899ca8813eb78e
Igor Petrov <fox0234@yandex.ru>
2018-12-11 23:07:24 +0300

    annotation for TaskEntity

--- src/main/java/com/yellow/photoshare/entity/TaskEntity.java
+++ src/main/java/com/yellow/photoshare/entity/TaskEntity.java
@@ -3,10 +3,16 @@ package com.yellow.photoshare.entity;
 import org.hibernate.search.annotations.Field;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
+
+
+@Entity(name = "TaskEntity")
+@Table(name="Task")
 public class TaskEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
